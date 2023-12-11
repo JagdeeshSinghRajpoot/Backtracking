ce5199052ec4a478664321c6b5941d22eecc48be
jagdeesh rajpoot <jagdeeshrajpoot9@gmail.com>
2023-12-11 21:53:22 +0530

    add test new java file for code test

--- findSubSet.java
+++ findSubSet.java
@@ -1,22 +1,24 @@
 public class findSubSet {
-    public static void findSSet(String str, String ans, int i){
-        if(i==str.length()){
-            if(ans.length() == 0){
+    public static void findSSet(String str, String ans, int i) {
+        if (i == str.length()) {
+            if (ans.length() == 0) {
                 System.out.println("null");
-            }else{
+            } else {
                 System.out.println(ans);
             }
             return;
         }
 
-        //Yes
-        findSSet(str, ans+str.charAt(i), i+1);
+        // Yes
+        findSSet(str, ans + str.charAt(i), i + 1);
 
-        //No
-        findSSet(str, ans, i+1);
+        // No
+        findSSet(str, ans, i + 1);
     }
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         findSSet("abc", "", 0);
+        // hello
     }
-    
+
 }
